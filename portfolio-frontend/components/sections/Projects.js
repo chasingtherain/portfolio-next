8bf1abd4359ece6bbc568bf3f32b36d8e5b8f261
ChasingRain <junpeng94@gmail.com>
2023-01-28 23:21:15 +0800

    added open source section

--- portfolio-frontend/components/sections/Projects.js
+++ portfolio-frontend/components/sections/Projects.js
@@ -7,10 +7,12 @@ function Projects() {
     return (
         <Element id='projects' name='projects'>
             <div className='my-36 md:mx-40 z-10'>
-                <p className='text-[28px] text-center mb-8 text-sky-300 font-semibold'>Some apps I have built: </p>
+                <p className='text-[28px] text-center mb-8 text-sky-300'>Some apps I have built</p>
                 <div className="flex flex-wrap justify-center gap-8">
                     {data.map((record, index) => <Project key={index} data={record}/>)}
                 </div>
+                <p className='text-[28px] text-center my-12 text-sky-300'>Open Source Contributions</p>
+                <p className='text-md text-center'>Coming soon</p>
             </div>
         </Element>
     )
