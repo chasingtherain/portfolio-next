9c771f6ed8414068716d6d49510b15fa0fa990e9
ChasingRain <junpeng94@gmail.com>
2023-03-27 22:06:16 +0800

    added courses to cs foundation section

--- portfolio-frontend/components/FoundationCard.js
+++ portfolio-frontend/components/FoundationCard.js
@@ -4,11 +4,11 @@ function FoundationCard({foundationData}) {
   return (
     <div className="card w-80 h-60 md:w-96 md:h-72 bg-base-200 shadow-xl z-0 mx-8 md:mx-0">
     <div className="card-body">
-        <h2 className="card-title my-4">{foundationData.title}</h2>
-        <p className='whitespace-pre-line'>{foundationData.description}</p>
-        {/* <div className="mt-2">
-            {data.stack.map((record, index) => <span key={index} className="badge text-slate-700 bg-sky-300 mx-1">{record}</span>)}
-        </div> */}
+        <h2 className="card-title my-1">{foundationData.title}</h2>
+        {!foundationData.courses.length && <p className='whitespace-pre-line'>{foundationData.description}</p>}
+        <div className="mt-1">
+            {foundationData.courses.map((record, index) => <a href={record.url} key={index} className="card mb-2 hover:text-sky-300 underline decoration-sky-400" target="_blank">{record.name}</a>)}
+        </div>
     </div>
 </div>
   )
--- portfolio-frontend/data/foundationData.js
+++ portfolio-frontend/data/foundationData.js
@@ -2,14 +2,24 @@ const foundationData = [
     {
     title: "Data Structure & Algo",
     description: "Coming soon",
+    courses: [
+      {name: "Data Structure & Algo by Abdul Bari", "url":"https://www.udemy.com/course/datastructurescncpp/"},
+    ]
     },
     {
     title: "Database",
     description: "Coming soon",
+    courses: [
+      {name: "SQL and PostgreSQL: Complete Developer's Guide", "url":"https://www.udemy.com/course/sql-and-postgresql/"},
+      {name: "Fundamentals of Database Engineering", "url":"https://www.udemy.com/course/database-engines-crash-course/"},
+    ]
     },
     {
     title: "System Design",
     description: "Coming soon",
+    courses: [
+      {name: "GreatFrontEnd System Design Prep", "url":"https://www.greatfrontend.com/"},
+    ]
     },
 ]
   
