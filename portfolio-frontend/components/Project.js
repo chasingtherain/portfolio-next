568fcd570298e8c5a097df7d712b4410376cb71b
ChasingRain <junpeng94@gmail.com>
2023-03-28 20:06:54 +0800

    added hover color change for buttons on project card

--- portfolio-frontend/components/Project.js
+++ portfolio-frontend/components/Project.js
@@ -13,12 +13,12 @@ function Project({data}) {
                             <FiFolder/>
                         </a>
                         <div className='flex gap-3 text-2xl mt-2'>
-                            {data.repo && <a className="text-3xl" href={data.repo} target="_blank" rel="noopener noreferrer"><FaGithub /></a>}
-                            {data.liveSite && <a className="text-3xl" href={data.liveSite} target="_blank" rel="noopener noreferrer"><FiExternalLink/></a>}
-                            {data.deck && <a className="text-3xl" href={data.deck} target="_blank" rel="noopener noreferrer"><BiLink /></a>}
+                            {data.repo && <a className="text-3xl hover:text-sky-300 cursor-pointer" href={data.repo} target="_blank" rel="noopener noreferrer"><FaGithub /></a>}
+                            {data.liveSite && <a className="text-3xl hover:text-sky-300 cursor-pointer" href={data.liveSite} target="_blank" rel="noopener noreferrer"><FiExternalLink/></a>}
+                            {data.deck && <a className="text-3xl hover:text-sky-300 cursor-pointer" href={data.deck} target="_blank" rel="noopener noreferrer"><BiLink /></a>}
                         </div>
                     </div>
-                    <h2 className="card-title my-4">{data.title}</h2>
+                    <h2 className="card-title my-2">{data.title}</h2>
                     <p className='whitespace-pre-line'>{data.description}</p>
                     <div className="mt-2">
                         {data.stack.map((record, index) => <span key={index} className="badge text-slate-700 bg-sky-300 mx-1">{record}</span>)}
