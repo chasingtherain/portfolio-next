44cd83edcdc74993bd48d017ae1dc70996c43f8e
ChasingRain <junpeng94@gmail.com>
2023-03-28 20:04:30 +0800

    added url link to houseform project

--- portfolio-frontend/components/sections/OpenSource.js
+++ portfolio-frontend/components/sections/OpenSource.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Element } from 'react-scroll';
+import { BiLink } from 'react-icons/bi'
 
 function OpenSource() {
-
+    const stack = ['Typescript','Zod','CI/CD','Automated testing']
     return (
         <div className=''>
             <Element name='opensource'>
@@ -14,14 +15,14 @@ function OpenSource() {
                         <a className="text-4xl">
                             <img src='/assets/open-source.png' className='text-5xl'/>
                         </a>
+                        <a className="hover:text-sky-300 cursor-pointer text-3xl" href="https://github.com/houseform" target="_blank" rel="noopener noreferrer">
+                            <BiLink/>
+                        </a>
                     </div>
                     <h2 className="card-title my-4">Houseform</h2>
                     <p className='whitespace-pre-line'>A field-first, Zod-powered, headless, runtime agnostic form validation library for React.</p>
-                    <p className=''>
-                        <span className='badge text-slate-700 bg-sky-300'>Typescript</span>
-                        <span className='badge text-slate-700 bg-sky-300 mx-1'>Zod</span>
-                        <span className='badge text-slate-700 bg-sky-300 mx-1'>CI/CD</span>
-                        <span className='badge text-slate-700 bg-sky-300 mx-1'>Automated testing</span>
+                    <p>
+                        {stack.map((record,index) => <span key={index} className='badge text-slate-700 bg-sky-300 mx-1'>{record}</span>)}
                     </p>
                 </div>
             </div>
