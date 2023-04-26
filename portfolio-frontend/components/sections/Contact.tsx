84d0715daabb466112c6f5514e59e873017d625d
ChasingRain <junpeng94@gmail.com>
2023-04-26 15:31:25 +0800

    converted Contact.js to Contact.tsx

--- portfolio-frontend/components/sections/Contact.tsx
+++ portfolio-frontend/components/sections/Contact.tsx
@@ -1,26 +1,33 @@
-import React, { useRef, useState } from 'react';
+import React, { FormEvent, useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 import { toast } from 'react-toastify';
 import { Element } from 'react-scroll'
 
 function Contact() {
-    const form = useRef();
+    const form = useRef<HTMLFormElement | null>(null);
 
     const[isDisabled, setIsDisabled] = useState(true)
     const[isLoading, setIsLoading] = useState("")
-    const[name, setName] = useState()
-    const[email, setEmail] = useState()
-    const[message, setMessage] = useState()
+    const[name, setName] = useState("")
+    const[email, setEmail] = useState("")
+    const[message, setMessage] = useState("")
 
 
-    const sendEmail = (e) => {
+    const sendEmail = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         if(name === "" || email === "") {
             return toast.warning("Fill in all blanks!")
+        
         }
+
+        const currentForm = form.current;
+        // this prevents sending emails if there is no form.
+        // in case currentForm cannot possibly ever be null,
+        if (currentForm == null) return toast.warning("Invalid form");
+
         setIsLoading("loading")
         console.log("email sent!")
-        emailjs.sendForm('service_3jcixii', 'template_718m4ml', form.current, '3bT_6TR-kXdoEnPD-')
+        emailjs.sendForm('service_3jcixii', 'template_718m4ml', currentForm, '3bT_6TR-kXdoEnPD-')
         .then((result) => {
             console.log(result.text);
             toast.success("Message sent!")
@@ -50,7 +57,7 @@ function Contact() {
                     <label className='mx-6'>Email</label>
                     <input className='input my-2 mx-6 bg-slate-50 rounded-md text-black' type="email" name="user_email" value={email} onChange={(e)=> setEmail(e.target.value)}/>
                     <label className='mx-6'>Message</label>
-                    <textarea className='bg-base-100 my-2 mx-6 bg-slate-50 rounded-md text-black' rows="3" name="message" value={message} onChange={(e)=> setMessage(e.target.value)} />
+                    <textarea className='bg-base-100 my-2 mx-6 bg-slate-50 rounded-md text-black' rows={3} name="message" value={message} onChange={(e)=> setMessage(e.target.value)} />
                     <button className={`btn bg-sky-300 text-white my-6 mx-6 rounded-md ${isLoading} hover:bg-green-700`} disabled={isDisabled} type="submit">
                         Send
                     </button>
